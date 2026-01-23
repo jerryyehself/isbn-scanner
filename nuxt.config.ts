15871cd6abd14cf552e29c721d0a8cdfb26d868c
jerryyehself <jerry40522@gmail.com>
2026-01-23 14:21:07 +0800

    fix: dev mode can open camera

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,25 @@
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
 export default defineNuxtConfig({
-	css: ['vuetify/styles'],
-	build: {
-		transpile: ['vuetify'],
-	},
-	vite: {
-		define: {
-			'process.env.DEBUG': false,
-		},
-	},
-	app: {
-		baseURL: '/isbn-scanner/',
-	},
-	nitro: {
-		preset: 'static',
-	},
+    css: ["vuetify/styles"],
+    build: {
+        transpile: ["vuetify"],
+    },
+    vite: {
+        define: {
+            "process.env.DEBUG": false,
+        },
+        plugins: [basicSsl()],
+    },
+    devServer: {
+        https: true,
+        host: "0.0.0.0", // 允許外網（手機）連線
+        port: 3000, // Nuxt 預設是 3000
+    },
+    app: {
+        baseURL: "/isbn-scanner/",
+    },
+    nitro: {
+        preset: "static",
+    },
 });
